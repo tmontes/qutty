e733093fe0931b957f7171f2df59edda334c85d6
Tiago Montes <tiago.montes@gmail.com>
2022-06-13 11:12:38 +0100

    setuptools packaging now declarative! :)

--- src/qutty/__init__.py
+++ src/qutty/__init__.py
@@ -1,15 +1,3 @@
 """
 QuTTY - A minimal GUI app to test pup.
 """
-
-__version__ = '1.1.0'
-
-__title__ = 'QuTTY'
-__description__ = 'QuTTY, a minimal GUI app to test pup'
-
-__license__ = 'MIT'
-__uri__ = 'https://github.com/tmontes/qutty'
-
-__author__ = 'Tiago Montes'
-__email__ = 'tiago.montes@gmail.com'
-
